1cb712ee5e88442725edb4c10f52f04e2192e043
zhangsong <shanchong@maizijf.com>
2017-05-18 14:50:54 +0800

    新增一个类

---
+++ src/main/java/cn/zsza/dailyTest/spring4/aop/Waiter.java
@@ -0,0 +1,10 @@
+package cn.zsza.dailyTest.spring4.aop;
+
+/**
+ * Created by zs on 2017/5/18.
+ * 11:28
+ */
+public interface Waiter {
+    void greetTo(String name);
+    void serveTo(String name);
+}
