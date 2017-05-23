0f08ba8c3488f4672826dd8533364b88e37976f0
zhangsong <shanchong@maizijf.com>
2017-05-23 11:18:15 +0800

    新增一个类

---
+++ src/main/java/cn/zsza/dailyTest/spring4/springAop/GreetingInterceptor.java
@@ -0,0 +1,27 @@
+package cn.zsza.dailyTest.spring4.springAop;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+/**
+ * Created by zs on 2017/5/23.
+ * 11:10
+ * 环绕增强
+ */
+public class GreetingInterceptor implements MethodInterceptor {
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+
+        Object args[] = invocation.getArguments();    // 目标方法入参
+
+        String clientName = (String) args[0];
+
+        System.out.println("How are you! Mr." + clientName + ".");
+
+        Object obj = invocation.proceed();
+
+        System.out.println("Please enjoy yourself!");
+
+        return obj;
+    }
+}
