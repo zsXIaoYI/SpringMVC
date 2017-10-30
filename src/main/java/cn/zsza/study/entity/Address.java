9137c4d5af578937c03a449a6d084a9ef857739f
zhangsong <zsza5466@163.com>
2017-10-30 21:14:52 +0800

    修改

---
+++ src/main/java/cn/zsza/study/entity/Address.java
@@ -0,0 +1,27 @@
+package cn.zsza.study.entity;
+
+/**
+ * Created by ZhangSong on 2016/9/26.
+ * 21:41
+ */
+public class Address {
+    private String province;
+    private String city;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+}
